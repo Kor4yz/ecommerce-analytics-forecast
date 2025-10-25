7d39a970cdfc1ee235ea4f5d233c5122ae45c62b
Kor4yz <147404339+Kor4yz@users.noreply.github.com>
2025-10-25 22:18:17 +0300

    Update 05_Обогащённые заказы.sql

--- sql/05_Обогащённые заказы.sql
+++ sql/05_Обогащённые заказы.sql
@@ -1,13 +1,18 @@
 CREATE OR REPLACE VIEW v_orders_enriched AS
 WITH first_buy AS (
-    SELECT user_id, min(order_date) AS first_order_date
-    FROM fact_orders
-    GROUP BY user_id
+  SELECT user_id, min(order_date) AS first_order_date
+  FROM fact_orders
+  GROUP BY user_id
 )
 SELECT
-    f.*,
-    fb.first_order_date,
-    toStartOfMonth(fb.first_order_date)           AS cohort_month,
-    (f.order_date > fb.first_order_date) ? 1 : 0  AS is_returning
-FROM fact_orders f
-LEFT JOIN first_buy fb USING (user_id);
+  f.order_id,
+  f.user_id,
+  f.order_ts,
+  f.order_date,
+  f.country,
+  f.revenue,
+  fb.first_order_date,
+  toStartOfMonth(fb.first_order_date)                  AS cohort_month,
+  if(f.order_date > fb.first_order_date, 1, 0)         AS is_returning
+FROM fact_orders AS f
+INNER JOIN first_buy AS fb USING (user_id);
